7c881b602e5daecde4063a7cec76f28062d2af13
mochbale <mochammadiqbal14081999@gmail.com>
2023-03-21 00:12:12 +0700

    Update Add Arrived nunggu clientjs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header.js";
 import Hero from "./components/Hero.js";
 import Browse from "./components/Browse.js";
+import Arrived from "./components/Arrived.js";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     < Header/>
     < Hero/>
     < Browse/>
+    < Arrived/>
     </>
   );
 }

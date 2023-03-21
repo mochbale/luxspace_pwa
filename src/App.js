159e57e2512b5ae104c651e832f8718453f1e7b3
mochbale <mochammadiqbal14081999@gmail.com>
2023-03-21 11:03:38 +0700

    Nambah Function Baru Asidemenu,client, update apps

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import Header from "./components/Header.js";
 import Hero from "./components/Hero.js";
 import Browse from "./components/Browse.js";
 import Arrived from "./components/Arrived.js";
+import Clients from "./components/Clients.js";
+import AsideMenu from "./components/AsideMenu.js";
 
 
 function App() {
   return (
     <>
-    < Header/>
-    < Hero/>
-    < Browse/>
-    < Arrived/>
+    <Header />
+    <Hero />
+    <Browse />
+    <Arrived />
+    <Clients />
+    <AsideMenu />
     </>
   );
 }
